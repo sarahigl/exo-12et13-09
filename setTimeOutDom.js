8cbdef959363330951b97a35ff22e2c7de8938a5
sarahigl <123654223+sarahigl@users.noreply.github.com>
2023-09-14 18:25:24 +0200

    exo settimeOut finis

---
+++ setTimeOutDom.js
@@ -0,0 +1,4 @@
+let body = document.getElementsByTagName('body');
+setTimeout( () => {//fonction permet définir un minuteur qui exécute une fonction ou un code donné après la fin du délai indiqué.
+    document.body.style.background = 'yellow';//modification apporter au document
+    }, "3000");//ou "3 secondes" = correspond au temps d'attente avant le changement 
