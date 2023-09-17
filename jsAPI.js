4d02e7ef51c842abdaf264c0f4297063893d0073
sarahigl <123654223+sarahigl@users.noreply.github.com>
2023-09-17 20:37:40 +0200

    Update jsAPI.js

--- jsAPI.js
+++ jsAPI.js
@@ -1,31 +1,25 @@
-
 //EXO api pokemon
-
-//tableau qui va stocker les element api
-let displayList = [];
+let list = document.getElementById("pokeListe");
+//let pokemonName = document.getElementById("name");
 //requête GET
-fetch(`https://pokeapi.co/api/v2/pokemon`)
-//extraction de données avec appel json sur objet respnse => envoie new promesse qui va résoudre les data json 
-.then(function(response){
-    return response.json()
-})
-// traitement data
-.then((data) => {
-    //creer var cible 
-    let list = document.getElementById("pokeListe");
-//on entre dans data et on extrait et on stock à chaque itération les nom des poke dans var puis ajoute sur le document pour l'afficher 
-    data.results.forEach((item) => {
-        displayList.push(item.name);
-        //créer un element li pour lister les noms
-        let li = document.createElement("li");
-        li.innerText = item.name;
-        list.appendChild(li);
-    })
-    console.log(displayList)
-})
-.catch((err) => {
+
+const contactApi1 =  async () => {
+    const requestString = "https://pokeapi.co/api/v2/pokemon";
+    const data = await fetch(requestString)
+    console.log(data)
+    //extraction de données avec appel json sur objet respnse => envoie new promesse qui va résoudre les data json 
+    let response = await data.json();
+    console.log(response);
+    const name = response.results;
+    const pokemonNames = name.map(pokemon => pokemon.name);//pokemon est un parametre il represente chaque element//ensuite avec .name on selectionne la colonne des name et on extrait chaque nom
+    const affichageHtml = pokemonNames.join("<br>");//pour faire en sorte que l'affichage se fasse pas sur une ligne.
+    list.innerHTML = affichageHtml;// le contenue de div et donc egale a la var qui fait l'affichage car elle meme correspond au nom pokemon mais en affichage modifié
+    //list.innerHTML = pokemonNames;// celui ci si onn souhaite afficher sans faire de modif sur notre visuel (list en ligne avec virgule par lisible)
+}
+const error = (err) => {
     console.log(`Error fetching: ${err}`)
-});
+};
+contactApi1();
 
 //cour
 const apiDiv = document.querySelector('.apiContact');
